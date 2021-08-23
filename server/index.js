6f8fd3e2c9135b04b30277833820efffd5162dc9
Melissa <lisastriker@gmail.com>
2021-08-23 13:16:06 +0800

    Changed server and env

--- server/index.js
+++ server/index.js
@@ -2,20 +2,20 @@ const accountSid = process.env.REACT_APP_TWILIO_ACCOUNT_SID
 // "ACd3be63050c5ec10eec96601af4f9cb1b"
 const authToken = process.env.REACT_APP_TWILIO_AUTH_TOKEN
 // "b0bb3d17c13fb8b93b1a83dbf4378e28"
-const client = require('twilio')("ACd3be63050c5ec10eec96601af4f9cb1b", "b0bb3d17c13fb8b93b1a83dbf4378e28");
+const client = require('twilio')(accountSid, authToken);
 const express = require('express'); 
 const cors = require('cors');
 const app = express()
 const path = require('path')
+require('dotenv').config({ path: path.join(__dirname, process.env.NODE_ENV)}); 
 const port = process.env.PORT || 4000
 app.use(cors())
+// const publicPath = path.join(__dirname, '..', 'public');
+//   app.use(express.static(publicPath))
+//   // app.get('*', (req, res) => {
+//   //   res.sendFile(path.join(publicPath, 'index.html'));
+//   // })
 
-if (process.env.NODE_ENV === 'production'){
-  app.use(express.static('build'))
-  app.get("*", (req, res) => {
-    req.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-  })
-}
 
 app.get('/send-text', (req, res) => {
   const { textMessage, recipient } = req.query
